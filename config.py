db0c6e055463b7fb74d0701c8746db357aedf888
Fahmi <fahmiardiansyah959@students.unnes.ac.id>
2023-06-22 00:57:20 +0700

    Last Fitur Seadanya

---
+++ config.py
@@ -0,0 +1,8 @@
+import os
+
+TOKEN = os.environ.get("TOKEN", "")
+ADMIN = int(os.environ.get("ADMIN", ""))
+OWNER = os.environ.get("OWNER", "usernamekamu")
+GROUP = os.environ.get("GROUP", "grup")
+CHANNEL = os.environ.get("CHANNEL", "channel")
+PROJECT_NAME = os.environ.get("PROJECT_NAME", "PRIVATE CHAT")
